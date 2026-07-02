2cd26224fcf5ecdc7b3a33714768c6af7453c356
hlyyy <1049556467@qq.com>
2026-07-02 10:34:07 +0800

    Strip layout chrome from 404 page

--- assets/Exception.f9d43104.js
+++ assets/Exception.f9d43104.js
@@ -1,7 +1,16 @@
-import{m as e,b as t}from"./index.24626119.js";
+import{m as e,b as t,j as s,Q as r}from"./index.24626119.js";
 const n=t({
   name:"ErrorPage",
   setup(){
+    const a=()=>document.body&&document.body.classList.add("simple-404-page");
+    const i=()=>document.body&&document.body.classList.remove("simple-404-page");
+    const c=()=>{
+      a();
+      document.querySelectorAll("header,footer,.lingtu-layout-header,.fixed.w-full.h-48px.bg-white.z-500.flex").forEach(d=>d.remove())
+    };
+    c();
+    s(c);
+    r(i);
     const o=()=>{location.href="/workspace"};
     return()=>e("main",{class:"simple-404"},[
       e("div",{class:"simple-404__code"},"404"),
